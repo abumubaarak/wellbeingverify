01e5a9ab23045e0b8f95618cf914dd6d1fd2330f
Abu Mubarak <ismaildeve070@gmail.com>
2024-12-26 00:14:00 +0100

    docs: update readme

---
+++ app/services/socket.ts
@@ -0,0 +1,6 @@
+import {io} from 'socket.io-client';
+
+// const socket = io('https://1e7a-197-211-58-127.ngrok-free.app');
+const socket = io('https://77c6-197-211-58-178.ngrok-free.app');
+
+export default socket;
